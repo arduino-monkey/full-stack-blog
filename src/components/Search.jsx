227878c13667ba6b1124b746c95cdbcf9a8a8963
arya <aryaanand1000@gmail.com>
2025-02-14 14:58:20 +0530

    single post page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
   };
 
   return (
-    <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
+    <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2 w-full">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -25,14 +25,15 @@ const Search = () => {
         height="20"
         fill="none"
         stroke="gray"
+        className="min-w-5"
       >
         <circle cx="10.5" cy="10.5" r="7.5" />
         <line x1="16.5" y1="16.5" x2="22" y2="22" />
       </svg>
       <input
         type="text"
-        placeholder="search a post..."
-        className="bg-transparent"
+        placeholder="search"
+        className="bg-transparent flex-1 min-w-0 text-sm"
         onKeyDown={handleKeyPress}
       />
     </div>
--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
+import Image from "../components/Image";
+import { Link } from "react-router-dom";
+import PostMenuActions from "../components/PostMenuActions";
+import Search from "../components/Search";
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
-  )
-}
+    <div className="mt-6">
+      {/*Details*/}
+      <div className="flex gap-8">
+        <div className="flex flex-col w-3/5 gap-8">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+            sint!
+          </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written By</span>
+            <Link className="text-blue-800">Arya Anand</Link>
+            <span>On</span>
+            <Link to="posts?cat=web-design" className="text-blue-800">
+              Web Design
+            </Link>
+            <span>2 days ago</span>
+          </div>
+          <p className="text-gray-400 font-medium">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+            soluta voluptate. Itaque autem quae assumenda. Quibusdam odit,
+            accusamus consectetur deleniti qui natus sunt optio porro magnam
+            fugiat quaerat adipisci sit.
+          </p>
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg" className="rounded-3xl object-cover" />
+        </div>
+      </div>
+      {/*Content*/}
+      <div className="flex flex-col md:flex-row gap-8 mt-8">
+        <div className="lg:text-lg flex flex-col gap-6 text-justify w-10/12">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptatem, perferendis quae? Consequatur, sed nemo quos non est
+            necessitatibus, facere repellat optio laudantium, natus architecto.
+            Eligendi eius id magni ipsum nobis, eveniet eum nulla eaque
+            quibusdam, officiis beatae, saepe illo iste possimus consequuntur
+            porro provident dolores repudiandae incidunt ea sequi doloribus!
+          </p>
+        </div>
+        {/*Side Menu*/}
+        <div className="px-4 h-max top-6 sticky w-2/12">
+          <h1 className="mt-4 mb-4 text-sm font-medium">Author</h1>
+          <div className="flex gap-4 items-center">
+            <Image
+              src="userImg.jpeg"
+              className="w-12 h-12 rounded-full object-cover"
+              w="48"
+              h="48"
+            />
+            <Link className="text-blue-800">Arya Anand</Link>
+          </div>
+          <p className="mt-4 text-sm text-gray-500">
+            Lorem ipsum dolor sit amet consectetur
+          </p>
+          <div className="flex gap-4 mt-4">
+            <Link>
+              <Image src="facebook.svg" />
+            </Link>
+            <Link>
+              <Image src="instagram.svg" />
+            </Link>
+          </div>
+          <PostMenuActions />
+          <h1 className="mt-4 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline" to="/">
+              All
+            </Link>
+            <Link className="underline" to="/">
+              Web Design
+            </Link>
+            <Link className="underline" to="/">
+              Development
+            </Link>
+            <Link className="underline" to="/">
+              Cars
+            </Link>
+            <Link className="underline" to="/">
+              AI
+            </Link>
+            <Link className="underline" to="/">
+              Watches
+            </Link>
+          </div>
+          <h1 className="mt-4 mb-4 text-sm font-medium">Search</h1>
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePostPage
+export default SinglePostPage;
